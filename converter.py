0978d5708d80b7162210f7c88c88f3d5e50a2f65
Mastella Simone <smastella@cogne.com>
2021-08-04 09:52:11 +0200

    fix, defined func for hh and mm

--- converter.py
+++ converter.py
@@ -7,8 +7,8 @@ def converter(cron_string):
         converted=""
         if (cron_string.count(' ')!=4):
             raise Exception('cron string','wrong format')
-        cron_minutes, cron_hour, cron_day, cron_month, cron_dow = cron_string.split(' ')
-        print(cron_minutes, cron_hour, cron_day, cron_month, cron_dow)
+        cron_minute, cron_hour, cron_day, cron_month, cron_dow = cron_string.split(' ')
+        print(cron_minute, cron_hour, cron_day, cron_month, cron_dow)
         
         #month
         converted+=cron_utils.month_converter(cron_month)
@@ -20,23 +20,7 @@ def converter(cron_string):
         converted+=cron_utils.day_converter(cron_dow= cron_dow, cron_day= cron_day)
 
         #hour:minutes
-        if(cron_hour.isnumeric() and cron_minutes.isnumeric()):
-            converted += ("at {:02d}:{:02d}".format(int(cron_hour),int(cron_minutes)))
-            return converted
-        else:
-
-            #hour
-            if(cron_hour == '*'):
-                converted += ("every hour, ")
-            elif(cron_hour.isnumeric()):
-                converted += ("at hour "+cron_day+", ")
-
-            #minute
-            if(cron_minutes == '*'):
-                converted += ("every minute")
-            elif(cron_minutes.isnumeric()):
-                converted += ("at minute "+cron_minutes)
-            return converted
+        return converted+cron_utils.hour_minute_converter(cron_hour=cron_hour, cron_minute=cron_minute)
     except:
         traceback.print_exc()
 
@@ -45,6 +29,7 @@ if __name__ == "__main__":
     # print(converter("4 * 5 7 *"))
     # print(converter("4 1 2 3 4"))
     print(converter("4 1 5-20/3,*/2 3 4"))
+    print(converter("4 2,8,*/5 * * *"))
     # print(converter("4 1 2 3 4"))
     # print(converter("* * * */2 2"))
     # print(converter("* * * 1,4,2,6 2"))
--- cron_utils.py
+++ cron_utils.py
@@ -39,6 +39,8 @@ def dow_converter(cron_dow, cron_day):
         if(cron_dow.isnumeric()):
             if (int(cron_dow)>=0 or int(cron_dow)<=7 ):
                 result += ("every "+days[int(cron_dow)]+" ") #OUTPUT
+        elif(cron_dow == "*"):
+            pass
         elif(',' in cron_dow):
             for current_day in sorted(cron_dow.split(',')):
                 result += (dow_converter(current_day, cron_day)+" and ") #OUTPUT
@@ -96,5 +98,86 @@ def day_converter(cron_day, cron_dow):
             result += ("from day "+start+" to day "+end+" every "+every+" days") #OUTPUT
     else: 
         raise Exception('day','error while parsing')
-    
+    return result #OUTPUT
+
+
+def hour_minute_converter(cron_hour, cron_minute):
+    result=""
+    if(cron_hour.isnumeric() and cron_minute.isnumeric()):
+            result += ("at {:02d}:{:02d}".format(int(cron_hour),int(cron_minute)))
+    else:
+        #hour
+        result+= hour_converter(cron_hour=cron_hour)
+        #minute
+        result+= minute_converter(cron_minute=cron_minute)
+    return result #OUTPUT
+
+def hour_converter(cron_hour):
+    result=""
+    if(cron_hour == '*'):
+        result+=("every hour, ")
+    elif(cron_hour == '*'):
+        pass
+    elif(cron_hour.isnumeric()):
+        if (int(cron_hour)>=0 or int(cron_hour)<=23 ):
+            result += ("at {:02d}".format(int(cron_hour))) #OUTPUT
+    elif(',' in cron_hour):
+        for current_hour in sorted(cron_hour.split(',')):
+            result += (hour_converter(current_hour)+" and ") #OUTPUT
+        result=result[:-5]+", " #OUTPUT
+    elif(cron_hour[:2] == "*/"):
+        cron_hour=cron_hour[2:]
+        if cron_hour.isnumeric() and int(cron_hour)>0:
+            if (int(cron_hour)>1):
+                result+= ("every {} hours".format(int(cron_hour)))#OUTPUT
+            else:
+                result+= ("every hour") #OUTPUT
+        else :
+            raise Exception('*/ hour','error while parsing')
+    elif("-" in cron_hour):
+        start,end=cron_hour.split("-")
+        if("/" not in end):
+            assert start.isnumeric() and end.isnumeric()
+            result += ("from hour {:02d} to hour {:02d}".format(int(start),int(end))) #OUTPUT
+        else:
+            end,every=end.split("/")
+            assert start.isnumeric() and end.isnumeric()
+            result += ("from hour {:02d} to hour {:02d} every {:02d} hours").format(int(start),int(end), int(every)) #OUTPUT
+    else: 
+        raise Exception('hour','error while parsing')
+    return result #OUTPUT
+
+def minute_converter(cron_minute):
+    result=""
+    if(cron_minute == '*'):
+        result+=("every minute, ")
+    elif(cron_minute == '*'):
+        pass
+    elif(cron_minute.isnumeric()):
+        if (int(cron_minute)>=0 or int(cron_minute)<=23 ):
+            result += ("at minute {:02d}".format(int(cron_minute))) #OUTPUT
+    elif(',' in cron_minute):
+        for current_hour in sorted(cron_minute.split(',')):
+            result += (hour_converter(current_hour)+" and ") #OUTPUT
+        result=result[:-5]+", " #OUTPUT
+    elif(cron_minute[:2] == "*/"):
+        cron_minute=cron_minute[2:]
+        if cron_minute.isnumeric() and int(cron_minute)>0:
+            if (int(cron_minute)>1):
+                result+= ("every {} minutes".format(int(cron_minute)))#OUTPUT
+            else:
+                result+= ("every minute") #OUTPUT
+        else :
+            raise Exception('*/ minute','error while parsing')
+    elif("-" in cron_minute):
+        start,end=cron_minute.split("-")
+        if("/" not in end):
+            assert start.isnumeric() and end.isnumeric()
+            result += ("from minute {:02d} to minute {:02d}".format(int(start),int(end))) #OUTPUT
+        else:
+            end,every=end.split("/")
+            assert start.isnumeric() and end.isnumeric()
+            result += ("from minute {:02d} to minute {:02d} every {:02d} minutes").format(int(start),int(end), int(every)) #OUTPUT
+    else: 
+        raise Exception('minute','error while parsing')
     return result #OUTPUT
